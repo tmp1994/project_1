3d797a3537383c0c2ee6e449cd94fbc7937153ff
Vitaly <vit-kanivec@ya.ru>
2013-03-11 00:47:25 +0400

    Initial Commit

---
+++ project_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  project_1
+//
+//  Created by Виталий on 11.03.13.
+//  Copyright (c) 2013 vitali0_projects. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
